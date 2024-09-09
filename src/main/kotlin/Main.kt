735aad7e2606389dcfbc76a301b95f82b2c10324
Not-Saint-Paul <pavelstankovsky@yandex.ru>
2024-09-09 20:11:50 +0700

    Кто это читает?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,7 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    println("Enter number below")
+    var number = readln()
+    println("First digit: " + number.first() + "\nLast digit: " + number.last())
 }
