e6e5fc9920b5de8cfafa5714392f79485c471a3b
Not-Saint-Paul <pavelstankovsky@yandex.ru>
2024-09-16 18:27:49 +0700

    Привет. А я тебя знаю)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,16 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    var usersNum = 0
+    var randNum = 1
+
+    while (usersNum != randNum) {
+        print("Enter number: ")
+        usersNum = readln().toInt()
+        randNum = (0..10).random()
+
+        if (usersNum < randNum) println("Your number is less then number to guess")
+        else if (usersNum > randNum) println("Your number is greater then number to guess")
+        else println("You guessed the number!")
+    }
 }
