c4bb001c8a01bed3e02e3d8d2dc68b32b50fda37
Not-Saint-Paul <pavelstankovsky@yandex.ru>
2024-09-16 18:55:48 +0700

    Купил на рынке мужик шляпу, а она ему как раз

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,18 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    var num = 1.0
+    var sum = 0.0
+    var mean = 0.0
+    var numberOfNumbers = 0
+
+    println("Enter numbers below (Enter 0 = stop input):")
+    while (num != 0.0) {
+        num = readln().toDouble()
+        sum += num
+        ++numberOfNumbers
+    }
+
+    mean = sum / numberOfNumbers
+    print("Number of numbers: $numberOfNumbers \nSum of numbers: $sum \nArithmetic mean of numbers: $mean")
 }
